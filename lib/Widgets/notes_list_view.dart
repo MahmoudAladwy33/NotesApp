237ca051d9480f6decd61a3120fb0e08cfb10203
MahmoudAladwy <mahmoudaladwy777@gmail.com>
2024-08-13 02:03:53 +0300

    create notes list with color

--- lib/Widgets/notes_list_view.dart
+++ lib/Widgets/notes_list_view.dart
@@ -4,14 +4,24 @@ import 'package:notes_app/Widgets/note_item.dart';
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
 
+  final data = const [
+    Color.fromARGB(255, 249, 105, 95),
+    Color.fromARGB(255, 252, 237, 98),
+    Color.fromARGB(255, 255, 189, 90),
+    Color.fromARGB(255, 125, 255, 129),
+    Color.fromARGB(255, 233, 111, 255),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: ListView.builder(
         itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(vertical: 8),
-            child:  NoteItem(),
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: NoteItem(
+              color: data[index % data.length],
+            ),
           );
         },
       ),
