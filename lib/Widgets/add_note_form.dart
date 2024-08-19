9b4b1aa2d28a601e8894553f03e2a5a1388e7472
MahmoudAladwy <mahmoudaladwy777@gmail.com>
2024-08-19 12:01:48 +0300

    add BlocConsumer to add note bottom sheet

---
+++ lib/Widgets/add_note_form.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/Widgets/custom_button.dart';
+import 'package:notes_app/Widgets/cutsom_text_field.dart';
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  String? title, content;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 32,
+          ),
+          CutsomTextField(
+            onSaved: (data) {
+              title = data;
+            },
+            hint: 'Title',
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          CutsomTextField(
+            onSaved: (data) {
+              content = data;
+            },
+            hint: 'Content',
+            maxLines: 5,
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+          CustomButton(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+        ],
+      ),
+    );
+  }
+}
