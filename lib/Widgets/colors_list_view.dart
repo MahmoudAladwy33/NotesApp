9787568beac72ac6119d9c9c422b18e2f13ec231
MahmoudAladwy <mahmoudaladwy777@gmail.com>
2024-08-21 13:07:25 +0300

    create colors list view done

--- lib/Widgets/colors_list_view.dart
+++ lib/Widgets/colors_list_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/Widgets/color_item.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 
 class ColorsListView extends StatefulWidget {
   const ColorsListView({super.key});
@@ -32,6 +34,7 @@ class _ColorsListViewState extends State<ColorsListView> {
             child: GestureDetector(
               onTap: () {
                 currentIndex = index;
+                BlocProvider.of<AddNoteCubit>(context).color = colors[index];
                 setState(() {});
               },
               child: ColorItem(
